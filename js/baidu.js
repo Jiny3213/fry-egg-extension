5221f5337b5c017a07254e3fe44a812359bedc9f
Jiny <149757089@qq.com>
2020-05-13 16:50:31 +0800

    update

--- js/baidu.js
+++ js/baidu.js
@@ -7,42 +7,33 @@ document.addEventListener('DOMContentLoaded', function() {
   let form = document.getElementById('form')
   let button = document.createElement('div')
 
-  // 在百度首页输入任意内容时, 页面样式会发生改变
-  kw.oninput = function() {
-    hasInput = true
+  if(location.pathname === '/') {
+    // 在百度首页输入任意内容时, 页面样式会发生改变
+    kw.oninput = function() {
+      hasInput = true
+      button.setAttribute('class', 'fe-btn fe-btn--normal bg s_btn')
+      form.appendChild(button)
+      kw.oninput = null
+    }
+  } else if(location.pathname === '/s') {
     button.setAttribute('class', 'fe-btn fe-btn--normal bg s_btn')
+    form.appendChild(button)
   }
+
 
   button.innerText = 'google 一下'
   button.onclick = function () {
     window.open(`https://www.google.com/search?q=${kw.value}`, '_blank')
   }
 
-  // 初始化类名
-  if(location.pathname === '/') {
-    button.setAttribute('class', 'fe-btn fe-btn--index btn self-btn bg s_btn')
-  } else {
-    button.setAttribute('class', 'fe-btn fe-btn--normal bg s_btn')
-  }
-
   // 原生交互体验
   button.onmouseleave = function() {
-    if(location.pathname === '/' && !hasInput) {
-      button.setAttribute('class', 'fe-btn fe-btn--index btn self-btn bg s_btn')
-    } else {
-      button.setAttribute('class', 'fe-btn fe-btn--normal bg s_btn')
-    }
+    button.setAttribute('class', 'fe-btn fe-btn--normal bg s_btn')
   }
   button.onmouseover = function() {
-    if(location.pathname === '/' && !hasInput) {
-      button.setAttribute('class', 'fe-btn fe-btn--index btn self-btn bg s_btn btnhover')
-    } else {
-      button.setAttribute('class', 'fe-btn fe-btn--normal bg s_btn btnhover')
-    }
+    button.setAttribute('class', 'fe-btn fe-btn--normal bg s_btn btnhover')
   }
 
-  // 添加到dom中
-  form.appendChild(button)
   // 去除广告
   removeAd()
 });
